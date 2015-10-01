0fb47f4bd7953b39299f99e91f3e08427b749f9d
Dan Boykis <danboykis@gmail.com>
2015-10-01 14:03:04 -0400

    Releasing 0.0.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.danboykis/lein-rpm "0.0.6-SNAPSHOT"
+(defproject com.danboykis/lein-rpm "0.0.6"
   :description "Create an RPM"
   :dependencies [[org.codehaus.mojo/rpm-maven-plugin "2.1.3"]
                  [org.clojure/java.data "0.1.1"]]
